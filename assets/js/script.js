cab4d71b0669ebde03b6bc3a9038558af839f8f5
Phil Branch <philbranch243@gmail.com>
2022-05-29 13:57:15 -0500

    message

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,23 @@
 // Define variables and DOM objects here
 var formE1 = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+
 // Define functions here
-var createTaskHandler = function(event) {
-    
+var createTaskHandler = function (event) {
+
     event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+
     tasksToDoEl.appendChild(listItemEl);
-    
+
 };
 
 formE1.addEventListener("submit", createTaskHandler);
