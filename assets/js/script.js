d6cb2b2e45a1ede4ca9970fc86404b68f4c35537
Phil Branch <philbranch243@gmail.com>
2022-05-29 10:02:32 -0500

    we can now add tasks to the board

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// Define variables and DOM objects here
+var buttonE1 = document.querySelector("#save-task");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+// Define functions here
+var createTaskHandler = function() {
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "This is a new task.";
+    tasksToDoEl.appendChild(listItemEl);
+};
+
+buttonE1.addEventListener("click", createTaskHandler);
